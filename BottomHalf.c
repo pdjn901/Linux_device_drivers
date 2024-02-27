3f23a82d59203aa3f6e4eb84758ab5f9ae0c686d
Jagadeesh Palli <64190675+pdjn901@users.noreply.github.com>
2024-02-27 15:37:00 +0530

    Create BottomHalf.c
    Embetronics bottom half device drivers, interrupts.

---
+++ BottomHalf.c
@@ -0,0 +1,7 @@
+//source from embetronics. 
+
+//https://embetronicx.com/tag/bottom-half/
+
+//covered topics 1.work queues, 2.task lets static method & dynamic method, 
+    3. Thread IRQ's,   4.Soft IRQ's. 
+    5. Interrupts. 
