ec267ee0e7880c873b7e6f545d5987b5bfb035fe
Jagadeesh Palli <64190675+pdjn901@users.noreply.github.com>
2024-02-27 15:29:56 +0530

    Update BootLoader_uart.c

--- BootLoader_uart.c
+++ BootLoader_uart.c
@@ -1,2 +1,3 @@
 Bootloader for STM32 using UART. 
 //https://embetronicx.com/tutorials/microcontrollers/stm32/bootloader/stm32-bootloader-uart-stm32-bootloader-tutorial/
+//https://github.com/Embetronicx/STM32-Bootloader/tree/ETX_Bootloader_2.0/Bootloader_Example
