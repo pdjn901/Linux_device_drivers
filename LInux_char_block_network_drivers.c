a8d8740a59bb9653b2970e5195f187e95bdd49d8
Jagadeesh Palli <64190675+pdjn901@users.noreply.github.com>
2024-06-24 19:44:03 +0530

    Update LInux_char_block_network_drivers.c
    [pdj: drivers]
         i) Char, block, network drivers with interrupts,ioctl, spinlocks.

--- LInux_char_block_network_drivers.c
+++ LInux_char_block_network_drivers.c
@@ -3,3 +3,13 @@ Char, block, network drivers with interrupts,ioctl, spinlocks.
 //char,block,network drivers
 https://linux-kernel-labs.github.io/refs/heads/master/labs/device_drivers.html
 
+
+  //debugging
+  https://linux-kernel-labs.github.io/refs/heads/master/lectures/debugging.html
+
+//system calls
+https://linux-kernel-labs.github.io/refs/heads/master/lectures/syscalls.html
+
+
+//memory management
+https://linux-kernel-labs.github.io/refs/heads/master/lectures/memory-management.html
